e48516ce7683227f273131a2d1a5774ec4714056
Sagar Gadave <sagargadave1234@gmail.com>
2025-01-04 17:40:17 +0530

    add WithoutService_src

---
+++ WithoutService_src/app/batch-details/batch-details.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-batch-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './batch-details.component.html',
+  styleUrl: './batch-details.component.css'
+})
+export class BatchDetailsComponent 
+{
+  public Batches = [
+    {"Name" : "PPA", "Duration" : "4 Months", "Fees" : 20000},
+    {"Name" : "LB", "Duration" : "4.5 Months", "Fees" : 21000},
+    {"Name" : "Angular", "Duration" : "4 Months", "Fees" : 21500}
+  ]
+}
